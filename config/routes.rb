e0a0955e7fb6fbc68deb7453afda475806764cb5
oreki3636 <127170175+oreki3636@users.noreply.github.com>
2023-04-12 03:21:58 +0000

    [Finish]4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :views
   devise_for :users
+
+  resources:post_images,only: [:new,:index,:show]
+
   root to:'homes#top'
   get "/homes/about"=>"homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
